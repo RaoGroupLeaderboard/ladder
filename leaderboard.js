6c8fcd35b06f6b4cf94ce89c245ed3441188d4ed
RaoGroupLeaderboard <leacolh@gmail.com>
2025-05-08 19:33:55 +0100

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -1,11 +1,1 @@
-fetch('data.json')
-  .then(res => res.json())
-  .then(data => {
-    const tbody = document.querySelector('#leaderboard tbody');
-    data.sort((a, b) => b.rating - a.rating);
-    data.forEach(player => {
-      const row = `<tr><td>${player.name}</td><td>${player.rating}</td></tr>`;
-      tbody.innerHTML += row;
-    });
-  });
 
